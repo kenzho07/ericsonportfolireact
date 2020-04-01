c501727ed7f84dd971d2749ab30c2e0f00b572d7
kenzho07 <ciriaco_07@yahoo.com>
2020-04-01 07:00:09 -0700

    Updates

---
+++ precache-manifest.db02446f5f100a2ecf5f5f667f387b38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cf6944ec4867e6adc87f7a39dcec1e",
+    "url": "/ericsonportfolireact/index.html"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/ericsonportfolireact/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e54cb844e8a1438bc0c6",
+    "url": "/ericsonportfolireact/static/css/main.6bdeedc7.chunk.css"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/ericsonportfolireact/static/js/2.0bc7f1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ericsonportfolireact/static/js/2.0bc7f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54cb844e8a1438bc0c6",
+    "url": "/ericsonportfolireact/static/js/main.0ca9276e.chunk.js"
+  },
+  {
+    "revision": "7d1142ea43fe78bb3a4b",
+    "url": "/ericsonportfolireact/static/js/runtime-main.e5c92337.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ericsonportfolireact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
