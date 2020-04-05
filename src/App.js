57438b1dda457026d461c7b3f97a32d9dec830fe
kenzho07 <ciriaco_07@yahoo.com>
2020-04-05 19:19:30 +0800

    Pages portfolio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import InfoPanel from './components/info-panel/InfoPanel';
-import DetailTabPanel from './components/detail-tabpanel/DetailTabPanel';
+import React, { Component } from "react";
+import InfoPanel from "./components/info-panel/InfoPanel";
+import DetailTabPanel from "./components/detail-tabpanel/DetailTabPanel";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <InfoPanel></InfoPanel>
-                <DetailTabPanel></DetailTabPanel>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='App'>
+        <InfoPanel></InfoPanel>
+        <DetailTabPanel></DetailTabPanel>
+      </div>
+    );
+  }
 }
 
 export default App;
